f6f66b340218de9dd4855a50857d72db863de1b6
Adrian <adriancharron@yahoo.ca>
2024-11-29 12:05:52 -0500

    Added route for movies with id

--- acharronH30A02/server.js
+++ acharronH30A02/server.js
@@ -3,9 +3,24 @@
 const express = require(`express`)
 const path = require(`path`)
 const app = express()
+const moviesFile = require(`./data/movies.json`)
 const PORT = 8888
 const WEBROOT = path.join(__dirname, `client`, `build`)
 app.use(express.static(WEBROOT))
+
+app.route(`/movies/:id?`).get((req, res) => {
+    if (req.params.id) {
+        let foundMovie = moviesFile.find((el) => el.Key === req.params.id)
+        if (foundMovie) {
+            res.json(foundMovie)
+        } else {
+            res.status(404)
+                .end(`<h1>Movie Not Found</h1>`)
+        }
+    } else {
+        res.json(moviesFile)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
